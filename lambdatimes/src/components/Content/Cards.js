236c71e4a2b70e7af01be15d6432b778127c4d59
Jamelle-Boose <19831942+Jamelle-Boose@users.noreply.github.com>
2019-06-03 18:02:25 -0700

    mvp complete

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Card from './Card';
 import PropTypes from 'prop-types'
 
+// Using the cards prop, map over the list creating a new Card component for each passing the card as the only prop
 const Cards = props => {
   return (
     <div className="cards-container">
